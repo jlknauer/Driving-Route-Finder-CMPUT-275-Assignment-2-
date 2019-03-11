00aa1dc225493ce57be3037083a172f87193e700
Jarrett Knauer <jarrettknauer@gmail.com>
2019-03-11 13:34:37 -0600

    finalized search fixes with heap.h

--- part_1/dijkstra.cpp
+++ part_1/dijkstra.cpp
@@ -4,7 +4,6 @@
 //
 // Assignment 2, Part 1: Driving Route Finder
 
-#include <list>
 #include <unordered_map>
 #include <utility>
 #include "dijkstra.h"
@@ -23,29 +22,24 @@
 //      none
 void dijkstra(const WDigraph& graph, int startVertex, std::unordered_map<int, PLI>& tree) {
     BinaryHeap<std::pair<int,int>, long long> events;
+    // start vertex shares predecessor to itself with a cost of 0
     events.insert(make_pair(startVertex,startVertex), 0);
 
     while (events.size() > 0) {
         int vertex = events.min().first.second;
         int predecessor = events.min().first.first;
         long long cost = events.min().second;
-        // std::cout << "Before: ";
-        // for (int i = 0; i < events.size(); ++i) {
-        //     std::cout << events.heap[i].second << ' ';
-        // }
-        events.popMin1();
-        // std::cout << "After: ";
-        // for (int i = 0; i < events.size(); ++i) {
-        //     std::cout << events.heap[i].second << ' ';
-        // }
-        // std::cout << '\n';
 
+        events.popMin();
+
+        // if the min key is already in the search tree, do nothing
         if (tree.find(vertex) != tree.end()) {
             continue;
         }
-
+        // else add to the search tree
         tree[vertex] = PLI(cost, predecessor);
 
+        // iterate through the vertex neighbours to begin searching
         for (auto i = graph.neighbours(vertex); i != graph.endIterator(vertex); ++i) {
             int neighbour_cost = cost + graph.getCost(vertex, *i);
             events.insert(make_pair(vertex, *i), neighbour_cost);
