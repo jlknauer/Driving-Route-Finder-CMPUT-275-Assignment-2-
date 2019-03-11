4b2c1d9843ce311dbcaa026360fcbbcb07c3a50c
Jarrett Knauer <jarrettknauer@gmail.com>
2019-03-11 02:48:29 -0600

    fixed popMin() as popMin1() in heap

--- part_1/dijkstra.cpp
+++ part_1/dijkstra.cpp
@@ -22,33 +22,33 @@
 // returns:
 //      none
 void dijkstra(const WDigraph& graph, int startVertex, std::unordered_map<int, PLI>& tree) {
-    BinaryHeap<int,PLI> events;
-    // at the beginning of the search, a vertex with ID == -1 indicates there is no predecessor
-    // the non-existent predecessor is assigned a cost of 0
-    events.insert(startVertex, PLI(0,-1));
+    BinaryHeap<std::pair<int,int>, long long> events;
+    events.insert(make_pair(startVertex,startVertex), 0);
 
-    while (events.size()>0) {
-        // minimum
-        int vertex = events.min().first;
-        int predecessor = events.min().second.second;
-        long long cost = events.min().second.first;
-        events.popMin();
-        // if the predecssor is already in the search tree, it has already been reached by an
-        // earlier search; do nothing
+    while (events.size() > 0) {
+        int vertex = events.min().first.second;
+        int predecessor = events.min().first.first;
+        long long cost = events.min().second;
+        // std::cout << "Before: ";
+        // for (int i = 0; i < events.size(); ++i) {
+        //     std::cout << events.heap[i].second << ' ';
+        // }
+        events.popMin1();
+        // std::cout << "After: ";
+        // for (int i = 0; i < events.size(); ++i) {
+        //     std::cout << events.heap[i].second << ' ';
+        // }
+        // std::cout << '\n';
+
         if (tree.find(vertex) != tree.end()) {
             continue;
         }
-        // else declare that the vertex has been reached for the cost associated with the
-        // predecssor
+
         tree[vertex] = PLI(cost, predecessor);
-        // begin searching all edges leaving the vertex
 
         for (auto i = graph.neighbours(vertex); i != graph.endIterator(vertex); ++i) {
-            int neighbour = *i;
-            // the search begins at the vertex with the current cost
-            // it will reach the neighbour totalling the current cost plus the edge distance
-            int neighbour_cost = cost + graph.getCost(vertex, neighbour);
-            events.insert(neighbour, PLI(neighbour_cost, vertex));
+            int neighbour_cost = cost + graph.getCost(vertex, *i);
+            events.insert(make_pair(vertex, *i), neighbour_cost);
         }
     }
 }
--- part_1/heap.h
+++ part_1/heap.h
@@ -24,13 +24,16 @@ public:
 
     // pop the minimum item from the heap
     void popMin();
+    void popMin1();
 
     // return the number of items held in the heap
     int size() const;
 
+    std::vector< std::pair<T,K> > heap;
+
 private:
     // the array holding the heap
-    std::vector< std::pair<T,K> > heap;
+
 };
 
 // Returns min element in heap by accessing the first
@@ -48,64 +51,111 @@ void BinaryHeap<T,K>::insert(const T& item, const K& key){
     // put it at the end
     heap.push_back(make_pair(item,key));
     // position of vertex
-    int v = heap.size();
-    v --;
-    int parent= (v-1)/2;
+    int v = heap.size() - 1;
+    int parent = (v-1)/2;
     // fix by swapping content of vertices;
     // repeat process until getting to the root or until achieving order
-    while(v>0 && heap[v].first < heap[parent].first){
+    while(v > 0 && heap[v].second < heap[parent].second){
         // swap with parent
-        pair<T,K> aux = heap[v];
-        heap[v]=heap[parent];
-        heap[parent]= aux;
+        std::pair<T,K> aux = heap[v];
+        heap[v] = heap[parent];
+        heap[parent] = aux;
         // move up the tree
         v = parent;
-        parent= (v-1)/2;
+        parent = (v-1)/2;
+    }
+}
+
+template <class T, class K>
+void BinaryHeap<T,K>::popMin1() {
+    heap.front() = heap.back();
+    heap.pop_back();
+    if (heap.size() == 0) return;
+
+    // root position
+    int v = 0;
+    int left = 2*v +1;
+    int right = 2*v + 2;
+
+    while ((left < heap.size() && heap[left].second < heap[v].second) ||
+            (right < heap.size() && heap[right].second < heap[v].second)) {
+        // temp tree node for swapping
+        pair<T,K> temp = heap[v];
+        // both left and right children exist
+        if (left < heap.size() && right < heap.size()) {
+            // left child is smaller of the two
+            if (heap[left].second < heap[right].second) {
+                heap[v] = heap[left];
+                heap[left] = temp;
+                v = left;
+            }
+            // right child is smaller of the two
+            else {
+                heap[v] = heap[right];
+                heap[right] = temp;
+                v = right;
+            }
+        }
+        // only left child exists
+        else if (left < heap.size() && right >= heap.size()) {
+            heap[v] = heap[left];
+            heap[left] = temp;
+            v = left;
+        }
+        // only right child exists
+        else if (left >= heap.size() && right < heap.size()) {
+            heap[v] = heap[right];
+            heap[right] = temp;
+            v = right;
+        }
+        // recompute left/right
+        left = 2*v + 1;
+        right = 2*v + 2;
     }
 }
 
 // remove the top and fix tree
 // based on pseudocode on the slides
 template <class T, class K>
-void BinaryHeap<T,K>::popMin(){
+void BinaryHeap<T,K>::popMin() {
     // assign the root the last vertex
-    heap.front()=heap.back();
+    heap.front() = heap.back();
     // remove the last vertex
     heap.pop_back();
     int s = heap.size();
     // make sure heap is not empty
-    if (s>0){
+    if (s > 0) {
         // current vertex position
         int v = 0;
-        int right =2*v + 2;
-        int left =2*v + 1;
+        int right = 2*v + 2;
+        int left = 2*v + 1;
         // while heap property is violated, children are smaller than parent
-        while(heap[right].first < heap[v].first or heap[left].first < heap[v].first){
+        while (heap[right].second < heap[v].second || heap[left].second < heap[v].second) {
             // create auxiliary vertex
             pair<T,K> aux = heap[v];
             // if left is the smallest
-            if(heap[right].first>heap[left].first){
-                //swap with left
-                heap[v]= heap[left];
-                heap[left]= aux;
+            if (heap[left].second < heap[right].second) {
+                // swap with left
+                heap[v] = heap[left];
+                heap[left] = aux;
                 // reassign v
                 v = left;
             }
             // if right is the smallest
-            else{
+            else {
                 // swap with right
-                heap[v]= heap[right];
-                heap[right]= aux;
+                heap[v] = heap[right];
+                heap[right] = aux;
                 // reassign v
                 v = right;
             }
             // recompute right and left
-            right =2*v + 2;
-            left =2*v + 1;
+            right = 2*v + 2;
+            left = 2*v + 1;
             // make sure we are not out of bounds
-            if (right > s-1){
+            if (right > s-1) {
                 // no children
-                if (left >s-1){
+                if (left > s-1) {
                     // no more possible swaps
                     // heap is ordered
                     return;
@@ -115,7 +165,6 @@ void BinaryHeap<T,K>::popMin(){
                     // go over the loop once more
                     right = left;
                 }
-
             }
         }
     }
